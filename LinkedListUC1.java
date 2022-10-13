cf364d58a45f4ee075d3b5118689142516394bb3
surajlangute <surajlangute55@gmail.com>
2022-10-13 17:07:16 +0530

    Created linkedlist

---
+++ LinkedListUC1.java
@@ -0,0 +1,15 @@
+package day14_linkedlist;
+
+import java.util.LinkedList;
+
+public class LinkedListUC1 {
+	public static void main(String[] args) {
+		LinkedList<Integer> lists = new LinkedList<>();
+		lists.add(56);
+		lists.add(30);
+		lists.add(70);
+
+		System.out.println(lists);
+
+	}
+}
